2db97e98e8513e38824585af2ba950fe8dac93bf
rob <liquidgod@hotmail.com>
2021-08-17 22:44:57 -0700

    updated app.js text - broke a test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,17 @@ function App() {
         </p>
         <p>
           Added this text on the feature branch.
+
+          - had to revert the link removal as it broke a test in Travis CI.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
